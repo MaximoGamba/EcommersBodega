0d30493779c93032b06155662ff6d2e2e544fffc
MaxiGamba <maximogamba64@gmail.com>
2026-04-08 11:09:40 -0300

    Implementar restricción de rol en la creación de categorías y actualizar la documentación con diagnóstico sobre la autorización de usuarios.

--- src/main/java/com/uade/tpo/demo/controllers/categories/CategoriesController.java
+++ src/main/java/com/uade/tpo/demo/controllers/categories/CategoriesController.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,6 +46,7 @@ public class CategoriesController {
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Object> createCategory(@RequestBody CategoryRequest categoryRequest)
             throws CategoryDuplicateException {
         Category result = categoryService.createCategory(categoryRequest.getDescription());
